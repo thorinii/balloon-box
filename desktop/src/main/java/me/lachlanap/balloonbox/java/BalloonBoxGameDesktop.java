70d7be8f5f5edfdc530fb576499634a8c39bea4b
ThorinII <flipsidelachy@gmail.com>
2013-07-06 11:50:13 +0930

    Initial LibGDX maven

---
+++ desktop/src/main/java/me/lachlanap/balloonbox/java/BalloonBoxGameDesktop.java
@@ -0,0 +1,14 @@
+package me.lachlanap.balloonbox.java;
+
+import me.lachlanap.balloonbox.core.BalloonBoxGame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class BalloonBoxGameDesktop {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useGL20 = true;
+		new LwjglApplication(new BalloonBoxGame(), config);
+	}
+}
